2fd6b22a0a167051dd2706e1a96b81b6de455d57
Nalina Udaiyakumar <nalina.u@gmail.com>
2022-08-03 16:04:13 -0400

    Update Dash app.py
    Update callbacks

--- src/Dash app.py
+++ src/Dash app.py
@@ -36,9 +36,9 @@ print(KWCGdoctors.shape)
 ## define function to calculate distance between 2 latitude and longitude points
 wgs84_geod = Geod(ellps='WGS84') #Distance will be measured on this ellipsoid - more accurate than a spherical method
 
-#Get distance between pairs of lat-lon points
+#Get distance between pairs of lat-long points
 def Distance(lat1,lon1,lat2,lon2):
-    az12,az21,dist = wgs84_geod.inv(lon1,lat1,lon2,lat2) #Yes, this order is correct
+    d1,d2,dist = wgs84_geod.inv(lon1,lat1,lon2,lat2) 
     return dist
 
 # Remove unwanted columns from KWCGdoctors df
@@ -56,72 +56,63 @@ KWCGdoctors = KWCGdoctors.drop(['Postalcode'],axis=1)
 print(KWCGdoctors.shape)
 print(KWCGdoctors.columns)
 
-
 # Initialise the app
 app = JupyterDash(__name__, external_stylesheets=[dbc.themes.FLATLY])
 
-# Define sidebar and content styles
-SIDEBAR_STYLE = {
-    'position': 'fixed',
-    'top': 0,
-    'left': 0,
-    'bottom': 0,
-    'width': '12rem',
-    'padding': '2rem 1rem',
-    'background-color': 'lightgray',
-}
-CONTENT_STYLE = {
-    'margin-left': '15rem',
-    'margin-right': '2rem',
-    'padding': '2rem' '1rem',
-}
-
 app.layout = dbc.Container([
         dbc.Row(
-            dbc.Col(html.H2('Family Doctor Search - KW region', className='text-center text-primary, mb-3'))),  # header row
+            dbc.Col(html.H2('Family Doctor Search - Kitchener Waterloo region', className='text-center text-primary, mb-3'))),  # header row
             
         dbc.Row([  # start of second row 
             dbc.Col([   # first column on second row - side bar
             dbc.Row([
                 dbc.Label("Show number of rows:"),
-                row_drop := dcc.Dropdown(value=15, clearable=False, style={'width':'35%'},
+                dcc.Dropdown(id="row_drop",value=15, clearable=False, style={'width':'35%'},
                                         options=[10,15,20,30]),
                 html.Hr()]),
             dbc.Row([
                 dbc.Label('Search by:'), #html.H4
-                mode_radio := dcc.RadioItems(
-                                        options = ['Location ', 'Distance '], 
-                                        value = 'Location ',
-                                        inline = False),
+                dcc.RadioItems(
+                    id="mode_radio", 
+                    options = ['Location', 'Distance'], 
+                    value = 'Location',
+                    inline = True   #style={'width': '35%'}
+                    ),
                 html.Hr()]),
             dbc.Row([    
                 dbc.Label('Select location'), #html.H6
-                location_radio := dcc.RadioItems(
-                                        options = ['Kitchener ', 'Waterloo ', 'Cambridge ', 'Guelph '], 
-                                        value = 'Kitchener ',
-                                        inline = False),
+                dcc.Dropdown(   #location_dropdown :=
+                    id="location_dropdown",
+                    options = region_options, 
+                    value = 'Kitchener',
+                    clearable = False),
                 html.Hr()]),
             dbc.Row([
-                dbc.Label('Enter postal code (ex. N2M0A1)'),  #html.H6
-                postalcode_input := dcc.Input(
-                    #                   id = 'postalcode_input',  # if Python 3.7 or lower, add this instead of using the walrus (:=) operator
-                                        type="text", 
-                                        placeholder="", 
-                                        debounce=True),
+                dbc.Label('Enter postal code (ex. N2M 0A1)'),  #html.H6
+                dcc.Input( #postalcode_input := 
+                    id = 'postalcode_input',  # if Python 3.8 or higher, add the walrus (:=) operator instead of defining id attribute
+                    type="text", 
+                    placeholder="N2M 0A1", 
+                    debounce=True),
                 dbc.Label('Select radius of search'),  #html.H6
-                radius_slider := dcc.Slider( 
-                           min=5, max=50, step=None,
-                           marks={5:"5", 10:"10", 15:"15", 20:"20", 30:"30", 50:"50"},
-                           value=0)]),
-#             html.Button('Search doctors', id='search_button', n_clicks=0)
-            ], width={'size': 2, 'offset': 0, 'order': 1}),  # width first column on second row
+                dcc.Slider( #radius_slider := 
+                   id="radius_slider",
+                   min=5, max=50, step=None,
+                   marks={5:"5", 10:"10", 15:"15", 20:"20", 30:"30", 50:"50"},
+                   value=0)]),
+                dbc.Button(id='download_button',
+                          children=["Download"],
+                          color="info",
+                          className="mt-1"),
+                dcc.Download(id="download-component")
+            ], width={'size': 2, 'offset': 0, 'order': 2}),  # width first column on second row
             
             dbc.Col([  # second column on second row - data table
             html.H5('List of doctors', className='text-left'),
             dt.DataTable(
                 id='doctors-datatable',
                 data=KWCGdoctors.to_dict('records'),
-                columns = [{'name':i, 'id':i} for i in KWCGdoctors.columns],
+                columns = [{'name':i, 'id':i} for i in KWCGdoctors.columns[0:7]],
                 editable=False,
                 page_size=15,
                 page_action='native',
@@ -133,40 +124,60 @@ app.layout = dbc.Container([
                 )
             ], width={'size': 9, 'offset': 0, 'order': 2}),  # width second column on second row
         ])  # end of second row        
-    ])
+    ], fluid=True)
 
-# # # Define callbacks
-@callback(
+# Define callbacks
+
+## Callback for filtering serch of doctors
+@app.callback(
     Output('doctors-datatable', 'data'),
-    Output('doctors-datatable', 'page_size'),       
+    Output('doctors-datatable', 'page_size'),
     Input('mode_radio','value'),
-    Input('location_radio', 'value'),
+    Input('location_dropdown', 'value'),
     Input('postalcode_input', 'value'),
     Input('radius_slider', 'value'),
     Input('row_drop', 'value')
 )
-def updateDoctorsTable(mode_v,location_v,postalcode_v,radius_v,row_v):
+def updateDoctorsTable(mode_v,location_v,postalcode_v,radius_v,row_v): #,num_clicks
+    global doctordf
+    
     doctordf = KWCGdoctors.copy()
     
-    if location_v:
-        doctordf = doctordf[doctordf['City'].isin(location_v)]
+    if mode_v=="Location":
+        if location_v:
+            doctordf = doctordf[doctordf['City'] == location_v]
 
-    if postalcode_v:
-        latgiven = KWCGpostalcodes[KWCGpostalcodes['Postalcode']==postalcode_v]['Latitude']
-        longgiven = KWCGpostalcodes[KWCGpostalcodes['Postalcode']==postalcode_v]['Longitude']
+    if mode_v=="Distance":
+        if postalcode_v:
+            # Gather latitude and longitude of the given postal code from the postal code database 
+            latgiven = KWCGpostalcodes[KWCGpostalcodes['Postalcode']==postalcode_v]['Latitude']
+            longgiven = KWCGpostalcodes[KWCGpostalcodes['Postalcode']==postalcode_v]['Longitude']
 
-        KWCGdoctors['LatGiven'] = latgiven
-        KWCGdoctors['LongGiven'] = longgiven                                                                        
-        KWCGdoctors['DistanceCol'] = df['dist'] = Distance(KWCGdoctors['Latitude'].tolist(),KWCGdoctors['Longitude'].tolist(),
-                                                          KWCGdoctors['LatGiven'].tolist(),KWCGdoctors['LongGiven'].tolist())
-        # sort KWCGdoctors in ascending order of DistanceCol
-        KWCGdoctors.sort_values(by="DistanceCol",ascending=True)
+            # Calculate distance of each doctor's location from the given postal code
+            KWCGdoctors['LatGiven'] = float(latgiven.values)
+            KWCGdoctors['LongGiven'] = float(longgiven.values)                                                                      
+            KWCGdoctors['DistanceCol'] =  Distance(KWCGdoctors['Latitude'].tolist(),KWCGdoctors['Longitude'].tolist(),
+                                            KWCGdoctors['LatGiven'].tolist(),KWCGdoctors['LongGiven'].tolist())
+            # floor and sort KWCGdoctors in ascending order of DistanceCol
+            KWCGdoctors["DistanceCol"] = (KWCGdoctors["DistanceCol"]/1000).apply(np.floor)
+            KWCGdoctors.sort_values(by="DistanceCol",ascending=True)
 
-        doctordf = KWCGdoctors[KWCGdoctors['DistanceCol']>=radius_v]
-        
+            doctordf = KWCGdoctors[KWCGdoctors['DistanceCol']<=radius_v]
+            doctordf.sort_values(by="DistanceCol", ascending=True)
+
     return doctordf.to_dict('records'), row_v
+
+## Callback for download option
+@app.callback(
+    Output('download-component', 'data'),
+    Input('download_button', 'n_clicks'),
+    prevent_initial_call=True,
+)
+def enableDownload(n_clicks): 
+    if n_clicks>0:
+        return dcc.send_data_frame(doctordf.to_excel, "Family doctor search_excel.xlsx", sheet_name="Sheet_name_1")
+
 
 # Run the app
 if __name__ == "__main__":
     app.run_server(debug=True, port=8058)
-    
